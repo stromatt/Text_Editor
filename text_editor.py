0e977dbbc3fa8fc22ceb00bd4ce19375cae8e879
Mattias <m.strom93@hotmail.com>
2025-07-16 21:41:37 +0200

    base configs

--- text_editor.py
+++ text_editor.py
@@ -30,26 +30,40 @@ def save_file(window, text_edit):
 def main():
     window = tk.Tk()
     window.title("Text editor")
-    window.rowconfigure(0, minsize=400)
-    window.columnconfigure(1, minsize=500)
+
+    # getting screen width and height of display
+    width = window.winfo_screenwidth()
+    height = window.winfo_screenheight()
+    # setting tkinter window size
+    window.geometry("%dx%d" % (width, height))
+    window.configure(bg="black")
 
     text_edit = tk.Text(
         window, font="Helvetica 18", bg="#303446")
-    text_edit.grid(row=0, column=1)
+    text_edit.pack(pady=40, padx=20, fill="both", expand=True)
+#    text_edit.pack(fill=tk.BOTH, expand=True)
 
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
     save_button = tk.Button(
-        frame, text="Save", command=lambda: save_file(window, text_edit))
+        frame, text="Save", command=lambda: save_file(window, text_edit),
+        bg="#303446", fg="white")
     open_button = tk.Button(
-        frame, text="Open", command=lambda: open_file(window, text_edit))
-
+        frame, text="Open", command=lambda: open_file(window, text_edit),
+        bg="#303446", fg="white")
     save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     open_button.grid(row=1, column=0, padx=5, sticky="ew")
-    frame.grid(row=0, column=0, sticky="ns")
+    frame.pack(pady=40, padx=20, anchor="nw", before=text_edit, side="left")
+
+    def select_all(event):
+        text_edit.tag_add(tk.SEL, "1.0", tk.END)
+        text_edit.mark_set(tk.INSERT, "1.0")
+        text_edit.see(tk.INSERT)
+        return 'break'
 
     window.bind("<Control-s>", lambda x: save_file(window, text_edit))
     window.bind("<Control-o>", lambda x: open_file(window, text_edit))
-
+    window.bind("<Control-Key-a>", select_all)
+    window.bind("<Control-Key-A>", select_all)
     window.mainloop()
 
 
